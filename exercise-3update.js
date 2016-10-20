a003ab8eca4fd5125236de75c59662ca8d0c93f6
Mary Scale <scalemary@gmail.com>
2016-10-20 15:07:49 +0000

    it's even prettier!

--- exercise-3update.js
+++ exercise-3update.js
@@ -24,13 +24,27 @@ promiseFuncs.query(nameQuery)
     
     result.map(function(row){
         var name = row['group_concat(ab.name)'].split(',');
+            //make our string of types an array so that we can meddle
+            //with it
+        name = name.map(function(type){
+            return indent + type;
+        })
+            //use a map inside of the map to give each string an
+            //indent in front of it
         row['group_concat(ab.name)'] = name;
+            //make the object value the new array with indents
         return [row.id, row.email, row['group_concat(ab.name)']];
+            //return all our data as an array
+            //so that we can use a forEach to console.log everything
     }).forEach(function(row){
-       
         console.log("#" + row[0] + " : " + row[1]);
+            //give our id values pound symbols and separate our
+            //email addresses with a colon
         console.log(row[2].join('\n'));
+            //join our array of strings with a new line
+            //as the separator
          console.log("");
+            //add a space in between each entry
     })
     // have the strings become an array so that they are grouped together
 })
