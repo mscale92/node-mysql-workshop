c52178e1ca0d13bb56124aed1dffa2f510223717
Mary Scale <scalemary@gmail.com>
2016-10-20 15:00:29 +0000

    made my output look prettier, time to find number 5

--- exercise-3update.js
+++ exercise-3update.js
@@ -15,18 +15,36 @@ var nameQuery = `
     from Account a 
     join AddressBook ab on (ab.accountId = a.id) 
     group by a.id`;
+//query
 
+var indent = '     ';
 
 promiseFuncs.query(nameQuery)
 .then(function(result){
     
-    var test = result.map(function(row){
+    result.map(function(row){
         var name = row['group_concat(ab.name)'].split(',');
         row['group_concat(ab.name)'] = name;
         return [row.id, row.email, row['group_concat(ab.name)']];
-    });
-    console.log(test);
+    }).forEach(function(row){
+       
+        console.log("#" + row[0] + " : " + row[1]);
+        console.log(row[2].join('\n'));
+         console.log("");
+    })
+    // have the strings become an array so that they are grouped together
 })
 .catch(function(err){
     console.log(err);
 })
+
+/*
+// Here is an example usage:
+  rows.forEach(function(row) {
+    console.log('#' + row.id + ': ' + row.email);
+  });
+  // This code will output lines like:
+  // #1: john@smith.com
+  // #2: abc@def.com
+  // #5: xx@yy.com
+*/
